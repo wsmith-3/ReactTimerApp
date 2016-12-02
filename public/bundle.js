c28c762b51486e73da71a9db889d777d33b1cf96
Willie Smith <wsmith@catalystdevworks.com>
2016-12-02 13:55:17 -0500

    update status to stop when count reaches zero

--- public/bundle.js
+++ public/bundle.js
@@ -26382,6 +26382,10 @@
 	      }
 	    }
 	  },
+	  componentWillUnmount: function componentWillUnmount() {
+	    clearInterval(this.timer);
+	    this.timer = undefined;
+	  },
 	  startTimer: function startTimer() {
 	    var _this = this;
 	
@@ -26390,6 +26394,9 @@
 	      _this.setState({
 	        count: newCount >= 0 ? newCount : 0
 	      });
+	      if (newCount === 0) {
+	        _this.setState({ countdownStatus: 'stopped' });
+	      }
 	    }, 1000);
 	  },
 	  handleSetCountdown: function handleSetCountdown(seconds) {
